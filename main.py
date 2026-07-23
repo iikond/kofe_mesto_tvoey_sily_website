82a117ec690f43b98eb3defade578730c2077883
iikond <idkondrahin@yandex.ru>
2026-07-23 14:19:03 +0300

    [SUPER MEGA ULTRA GLOBAL UPDATE] Refactor project structure, add backend and fix static assets

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+ 
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+ 
+@app.get("/")
+def root():
+    return FileResponse("templates/index.html")
